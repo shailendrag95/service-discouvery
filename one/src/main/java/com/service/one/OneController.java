fb2bb255a1b1b2f182fd6381d752283de3ad1784
shailendrag95 <shailendrag95@gmail.com>
2024-03-04 17:39:28 +0530

    cloud configuration

--- one/src/main/java/com/service/one/OneController.java
+++ one/src/main/java/com/service/one/OneController.java
@@ -1,5 +1,7 @@
 package com.service.one;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,19 @@ import java.net.http.HttpResponse;
 
 @RestController
 @RequestMapping("/one")
+@RefreshScope
 public class OneController {
+
+    @Value("${server.nick.name:default}")
+    private String nickName;
 
     @GetMapping("/health/status")
     public ResponseEntity<String> healthStatus(){
         return ResponseEntity.ok().body("UP");
     }
+
+    @GetMapping("/nick-name")
+    public ResponseEntity<String> getNickName(){ return ResponseEntity.ok(nickName);}
 }
 
 
